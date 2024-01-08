1de2ffe2e7095f4069b19a58f2eeb25299e427e4
Eddev <eduardemian777@gmail.com>
2024-01-08 18:02:03 -0300

    feat: :sparkles: finish footer + logic year

--- src/main.js
+++ src/main.js
@@ -1,45 +1,48 @@
-import "../sass/main.scss";
+import '../sass/main.scss';
 
-//  SCROLL SECTIONS ACTIVE LINK
+//  Muestra el color de los active-link al hacer scroll vertical, importante dejar los id de nav-icon, ya que cambia el color en formato mobile
+const sections = document.querySelectorAll('section[id]');
 
-const sections = document.querySelectorAll('section[id]')
+function scrollActive() {
+    const scrollY = window.scrollY;
 
-function scrollActive(){
-    const scrollY = window.scrollY
-
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
             sectionTop = current.offsetTop - 50,
-            sectionId = current.getAttribute('id')
+            sectionId = current.getAttribute('id');
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document
+                .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.add('active-link');
+        } else {
+            document
+                .querySelector('.nav__menu a[href*=' + sectionId + ']')
+                .classList.remove('active-link');
         }
-    })
+    });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener('scroll', scrollActive);
 
+// Desactiva la funcion de descargar en el video
+document.addEventListener('DOMContentLoaded', function () {
+    var miVideo = document.getElementById('miVideo');
+    miVideo.controlsList = 'nodownload noremoteplayback';
+});
 
-//  CHANGE BACKGROUND HEADER
+// Genera en HTML el año actual, asi evito cambiarlo cada año.
+document.addEventListener('DOMContentLoaded', function () {
+    var year = new Date().getFullYear();
+    document.getElementById(
+        'copyright',
+    ).innerHTML = `<span class="footer-copyright__span"> © ${year} ∾ </span>
+                <span class="footer-copyright__span footer-copyright__span--color"><a href="https://bydiamondgod.github.io/" target="_blank"> Eddev </a></span>`;
+});
 
+//  Hace un sombreado en el header al hacer scroll vertical
 /*  function scrollHeader(){
      const header = document.getElementById('header')
 
      if(this.scrollY >= 70) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
  }
  window.addEventListener('scroll', scrollHeader) */
-
-
- 
-/*  let myVideo = document.getElementById("myVideo");
-
- window.playVideo = function(file) {
-    myVideo.src = file
-} */
-
-  document.addEventListener('DOMContentLoaded', function () {
-    var miVideo = document.getElementById('miVideo');
-    miVideo.controlsList = "nodownload noremoteplayback";
-  });
